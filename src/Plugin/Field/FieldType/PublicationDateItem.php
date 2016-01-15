8597487be7d08849eef27c673b0bbe6279645aa4
Florian Weber <florian.weber@ueberbit.de>
2016-01-15 15:34:56 +0100

    Add simple publication date widget

--- src/Plugin/Field/FieldType/PublicationDateItem.php
+++ src/Plugin/Field/FieldType/PublicationDateItem.php
@@ -36,7 +36,6 @@ class PublicationDateItem extends ChangedItem {
    * {@inheritdoc}
    */
   public function applyDefaultValue($notify = TRUE) {
-    parent::applyDefaultValue($notify);
     $value = $this->isPublished() ? REQUEST_TIME : NULL;
     $published_at_or_now = isset($value) ? $value : REQUEST_TIME;
     $this->setValue(['value' => $value, 'published_at_or_now' => $published_at_or_now], $notify);
@@ -69,25 +68,6 @@ class PublicationDateItem extends ChangedItem {
     // Set the timestamp to request time if it is not set.
     if (!$this->value) {
       $this->value = REQUEST_TIME;
-    }
-    else {
-      // On an existing entity translation, the changed timestamp will only be
-      // set to the request time automatically if at least one other field value
-      // of the entity has changed. This detection does not run on new entities
-      // and will be turned off if the changed timestamp is set manually before
-      // save, for example during migrations or by using
-      // \Drupal\content_translation\ContentTranslationMetadataWrapperInterface::setChangedTime().
-      /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-      $entity = $this->getEntity();
-      /** @var \Drupal\Core\Entity\ContentEntityInterface $original */
-      $original = $entity->original;
-      $langcode = $entity->language()->getId();
-      if (!$entity->isNew() && $original->hasTranslation($langcode)) {
-        $original_value = $original->getTranslation($langcode)->get($this->getFieldDefinition()->getName())->value;
-        if ($this->value == $original_value && $entity->hasTranslationChanges()) {
-          $this->value = REQUEST_TIME;
-        }
-      }
     }
   }
 
